fbf6ac39864ec2412d26bd3670c1e8edb0cfc0d3
Kelly Smith-Holbourn <kelly.smith.holbourn@gmail.com>
2011-08-28 11:45:30 +1000

    Removed aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
-AWS.config({
-    :access_key_id => ENV['S3_KEY'],
-    :secret_access_key => ENV['S3_SECRET']
-  })
+#AWS.config({
+ #   :access_key_id => ENV['S3_KEY'],
+  #  :secret_access_key => ENV['S3_SECRET']
+  #})
