1a50e2adedeef4b8acbafb71e58174f7e92a3987
Kelly Smith-Holbourn <kelly.smith.holbourn@gmail.com>
2011-10-02 18:23:01 +1100

    Fixed error in s3 again

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,5 +1,4 @@
 AWS::S3::Base.establish_connection!(
   :access_key_id     => ENV['S3_KEY'],
-  :secret_access_key => ENV['S3_SECRET'],
-  :bucket => 'media.kellysmithholbourn.com'
+  :secret_access_key => ENV['S3_SECRET']
 )
