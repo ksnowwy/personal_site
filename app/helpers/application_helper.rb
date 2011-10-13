265b4da43ac1a31b901739ba8d04daa33494684d
Kelly Smith-Holbourn <kelly.smith.holbourn@gmail.com>
2011-10-13 17:11:16 +1100

    Changed title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Return a title on a per-page basis.
   def title
-    base_title = "Kelly Smith-Holbourn"
+    base_title = "Morning-Water-196"
     if @title.nil?
       base_title
     else
